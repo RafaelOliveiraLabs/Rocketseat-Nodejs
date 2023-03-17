bcf9c18c392a8a07adbddf66cf9782d4ea65c8d5
rafamoliv <rafaoliv.ti@gmail.com>
2023-03-17 16:04:29 -0300

    feat: completion of module 5 - routes

--- src/server.js
+++ src/server.js
@@ -1,32 +1,25 @@
 import http from "node:http";
-import { randomUUID } from "node:crypto";
-import { Database } from "./database.js";
 import { json } from "./middlewares/json.js";
-
-const database = new Database();
+import { routes } from "./routes.js";
+import { extractQueryParams } from "./utils/extract-query-params.js";
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
 
   await json(req, res);
 
-  if (method === "GET" && url === "/users") {
-    const users = database.select("users");
-    return res.end(JSON.stringify(users));
-  }
+  const route = routes.find((route) => {
+    return route.method === method && route.path.test(url);
+  });
 
-  if (method === "POST" && url === "/users") {
-    const { name, email } = req.body;
+  if (route) {
+    const routeParams = req.url.match(route.path);
+    const { query, ...params } = routeParams.groups;
 
-    const user = {
-      id: randomUUID(),
-      name: name,
-      email: email,
-    };
+    req.params = params;
+    req.query = query ? extractQueryParams(query) : {};
 
-    database.insert("users", user);
-
-    return res.writeHead(201).end();
+    return route.handler(req, res);
   }
 
   return res.writeHead(404).end();
